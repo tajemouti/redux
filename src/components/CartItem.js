94cfd9890586c703db46916315805a6b7ee07dcf
tajemouti <tajemouti@gmail.com>
2023-08-30 12:24:19 +0100

    Dispatch remove/increase/decrease actions

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
+import { useDispatch } from 'react-redux';
+import { decrease, increase, removeItem } from '../features/cart/cartSlice';
 
 const CartItem = ({ id, img, title, price, amount }) => {
-  return (
-    <article className='cart-item'>
-      <img src={img} alt={title} />
-      <div>
-        <h4>{title}</h4>
-        <h4 className='item-price'>${price}</h4>
-        <button className='remove-btn'>remove</button>
-      </div>
-      <div>
-        <button className='amount-btn'>
-          <ChevronUp />
-        </button>
-        <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
-          <ChevronDown />
-        </button>
-      </div>
-    </article>
-  );
+    const dispatch = useDispatch();
+    return (
+        <article className='cart-item'>
+            <img src={img} alt={title} />
+            <div>
+                <h4>{title}</h4>
+                <h4 className='item-price'>${price}</h4>
+                <button
+                    className='remove-btn'
+                    onClick={() => {
+                        dispatch(removeItem(id));
+                    }}
+                >
+                    remove
+                </button>
+            </div>
+            <div>
+                <button
+                    className='amount-btn'
+                    onClick={() => {
+                        dispatch(increase({id}));
+                    }}
+                >
+                    <ChevronUp />
+                </button>
+                <p className='amount'>{amount}</p>
+                <button 
+                className='amount-btn'
+                onClick={() => {
+                    dispatch(decrease({id}));
+                }}
+                >
+                    <ChevronDown />
+                </button>
+            </div>
+        </article>
+    );
 };
 
 export default CartItem;
