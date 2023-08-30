7e321dfc44c5ff187829143f619a430ececcc029
tajemouti <tajemouti@gmail.com>
2023-08-30 11:47:15 +0100

    Add action to clear button

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,41 +1,48 @@
 import React from 'react';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
-  const { cartItems, total, amount } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    const { cartItems, total, amount } = useSelector((state) => state.cart);
 
-  if (amount < 1) {
+    if (amount < 1) {
+        return (
+            <section className='cart'>
+                <header>
+                    <h2>your bag</h2>
+                    <h4 className='empty-cart'>is currently empty</h4>
+                </header>
+            </section>
+        );
+    }
     return (
-      <section className='cart'>
-        <header>
-          <h2>your bag</h2>
-          <h4 className='empty-cart'>is currently empty</h4>
-        </header>
-      </section>
+        <section className='cart'>
+            <header>
+                <h2>your bag</h2>
+            </header>
+            <div>
+                {cartItems.map((item) => {
+                    return <CartItem key={item.id} {...item} />;
+                })}
+            </div>
+            <footer>
+                <hr />
+                <div className='cart-total'>
+                    <h4>
+                        total <span>${total}</span>
+                    </h4>
+                </div>
+                <button
+                    className='btn clear-btn'
+                    onClick={() => dispatch(clearCart())}
+                >
+                    clear cart
+                </button>
+            </footer>
+        </section>
     );
-  }
-  return (
-    <section className='cart'>
-      <header>
-        <h2>your bag</h2>
-      </header>
-      <div>
-        {cartItems.map((item) => {
-          return <CartItem key={item.id} {...item} />;
-        })}
-      </div>
-      <footer>
-        <hr />
-        <div className='cart-total'>
-          <h4>
-            total <span>${total}</span>
-          </h4>
-        </div>
-        <button className='btn clear-btn'>clear cart</button>
-      </footer>
-    </section>
-  );
 };
 
 export default CartContainer;
