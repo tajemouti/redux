d273841063983d43c0b42a9c847aff82170db225
tajemouti <tajemouti@gmail.com>
2023-08-30 14:09:25 +0100

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useEffect } from "react";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
 function App() {
-  const { isOpen } = useSelector((store) => store.modal);
+  const { isOpen, isLoading } = useSelector((store) => store.modal);
   const { cartItems } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  };
 
   return (
     <main>
